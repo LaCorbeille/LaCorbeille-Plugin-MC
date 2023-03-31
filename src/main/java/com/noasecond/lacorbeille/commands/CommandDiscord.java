a5b2f00d15852cff2ece9399463ba7d9f74ed9b9
ElloWorld01 <elloworld@mailo.com>
2023-03-31 10:07:35 +0200

    :lipstick: Enhance UI

--- src/main/java/com/noasecond/lacorbeille/commands/CommandDiscord.java
+++ src/main/java/com/noasecond/lacorbeille/commands/CommandDiscord.java
@@ -1,8 +1,10 @@
 package com.noasecond.lacorbeille.commands;
 
+import org.bukkit.Sound;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class CommandDiscord implements CommandExecutor {
 
@@ -10,7 +12,9 @@ public class CommandDiscord implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
         if (!cmd.getName().equalsIgnoreCase("discord"))
             return false;
-        sender.sendMessage("https://discord.gg/hmPzS4k");
+        sender.sendMessage("Rejoins le Discord §lLaCorbeille §r§9§uhttps://discord §9§u.gg/hmPzS4k §r§f!");
+        if (sender instanceof Player p)
+            p.playSound(p.getLocation(), Sound.BLOCK_AMETHYST_BLOCK_BREAK, 1, 1);
         return true;
     }
 }
--- src/main/java/com/noasecond/lacorbeille/commands/CommandLobby.java
+++ src/main/java/com/noasecond/lacorbeille/commands/CommandLobby.java
@@ -1,6 +1,7 @@
 package com.noasecond.lacorbeille.commands;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Sound;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -12,8 +13,9 @@ public class CommandLobby implements CommandExecutor {
         if (!cmd.getName().equalsIgnoreCase("lobby"))
             return false;
         if (sender instanceof Player p) {
-            p.sendMessage("Vous avez été téléporté au spawn du lobby.");
+            p.sendMessage("§aVous avez été téléporté au spawn du lobby !");
             p.teleport(Bukkit.getWorld("lobby").getSpawnLocation());
+            p.playSound(p.getLocation(), Sound.ENTITY_FOX_TELEPORT, 1, 1);
         }
         return true;
     }
