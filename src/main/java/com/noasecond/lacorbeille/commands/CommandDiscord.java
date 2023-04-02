beb9cfba5cf0ccf57f287dcc504b54328bdb6d5e
ElloWorld01 <elloworld@mailo.com>
2023-04-02 17:52:01 +0200

    :lipstick: Add click and hover to the /discord command result

--- src/main/java/com/noasecond/lacorbeille/commands/CommandDiscord.java
+++ src/main/java/com/noasecond/lacorbeille/commands/CommandDiscord.java
@@ -1,5 +1,10 @@
 package com.noasecond.lacorbeille.commands;
 
+import net.md_5.bungee.api.ChatColor;
+import net.md_5.bungee.api.chat.ClickEvent;
+import net.md_5.bungee.api.chat.HoverEvent;
+import net.md_5.bungee.api.chat.TextComponent;
+import net.md_5.bungee.api.chat.hover.content.Text;
 import org.bukkit.Sound;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -12,9 +17,17 @@ public class CommandDiscord implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
         if (!cmd.getName().equalsIgnoreCase("discord"))
             return false;
-        sender.sendMessage("Rejoins le Discord §lLaCorbeille §r§9§uhttps://discord §9§u.gg/hmPzS4k §r§f!");
-        if (sender instanceof Player p)
-            p.playSound(p.getLocation(), Sound.BLOCK_AMETHYST_BLOCK_BREAK, 1, 1);
+        if (!(sender instanceof Player p))
+            return false;
+        ChatColor orangeForLink = ChatColor.of("#fd9644");
+
+        TextComponent discord = new TextComponent("Rejoins le Discord §lLaCorbeille §r" +
+                orangeForLink+"§l§9https://discord.gg/hmPzS4k" +
+                " §r§f!");
+        discord.setClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, "https://discord.gg/hmPzS4k"));
+        discord.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new Text("§bhttps://discord.gg/hmPzS4k")));
+        p.spigot().sendMessage(discord);
+        p.playSound(p.getLocation(), Sound.BLOCK_AMETHYST_BLOCK_BREAK, 1, 1);
         return true;
     }
 }
