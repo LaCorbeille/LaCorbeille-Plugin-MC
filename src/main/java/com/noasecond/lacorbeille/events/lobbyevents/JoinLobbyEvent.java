666494890cd6b5cc113a15e9207dddfba28056ea
YoruKiwi <contact@noasecond.com>
2023-04-08 23:45:02 +0200

    :lipstick: Update book

--- src/main/java/com/noasecond/lacorbeille/events/lobbyevents/JoinLobbyEvent.java
+++ src/main/java/com/noasecond/lacorbeille/events/lobbyevents/JoinLobbyEvent.java
@@ -23,8 +23,15 @@ public class JoinLobbyEvent implements Listener {
         ItemStack welcomeBook = new ItemStack(Material.WRITTEN_BOOK);
         BookMeta bookMeta = (BookMeta) welcomeBook.getItemMeta();
         bookMeta.setTitle("Bienvenue !");
-        bookMeta.setAuthor("YoruKiwi");
-        bookMeta.addPage("Bienvenue sur La Corbeille !");
+        bookMeta.setAuthor("Yoru_Kiwi");
+        bookMeta.addPage("Bienvenue dans,\n" +
+                                "§5§k!!!§5 §3La Corbeille§5§k !!!§0\n" +
+                                "\n" +
+                                "Sommaire :\n" +
+                                "contributeurs ---- p1\n" +
+                                "infos ----------- p2\n" +
+                                "commandes ------ p3\n" +
+                                "règles ---------- p4");
         welcomeBook.setItemMeta(bookMeta);
         if (player.getInventory().getItem(8) == null)
             player.getInventory().setItem(8, welcomeBook);
