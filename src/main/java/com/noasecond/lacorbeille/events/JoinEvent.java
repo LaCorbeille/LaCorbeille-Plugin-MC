98b8ea373e1fcdea3a49f7e747e0b6c1279937f2
YoruKiwi <contact@noasecond.com>
2023-04-14 11:37:32 +0200

    :sparkles: Add welcome title

--- src/main/java/com/noasecond/lacorbeille/events/JoinEvent.java
+++ src/main/java/com/noasecond/lacorbeille/events/JoinEvent.java
@@ -17,11 +17,25 @@ public class JoinEvent implements Listener {
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
         Player player = event.getPlayer();
+
+        //PlayerColor
         ConfigManager configManager = new ConfigManager(plugin, "colors.yml");
         ChatColor color = configManager.getPlayerColor(player);
         if (color != null) {
             player.setDisplayName(color + player.getName() + ChatColor.RESET); //Color in tchat
             player.setPlayerListName(color + player.getName()); //Color in tablist
         }
+
+        //WelcomTitle
+        String welcomeTitle = "§6La Corbeille";
+        String welcomeSubtitle = "§evous souhaite la bienvenue !";
+        if (player.hasPlayedBefore()) {
+            welcomeTitle = "§6La Corbeille";
+            welcomeSubtitle = "§evous souhaite une bonne partie !";
+        }
+        int fadeInTime = 10;
+        int stayTime = 100;
+        int fadeOutTime = 10;
+        player.sendTitle(welcomeTitle, welcomeSubtitle, fadeInTime, stayTime, fadeOutTime);
     }
 }
